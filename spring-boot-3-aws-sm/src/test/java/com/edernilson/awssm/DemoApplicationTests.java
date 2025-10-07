6591dca6496eea3898f5aecb73df3b503eb425f4
Eder Nilson <eder.nilson@pathfind.com.br>
2025-10-07 20:37:14 -0300

    Initial version

---
+++ spring-boot-3-aws-sm/src/test/java/com/edernilson/awssm/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edernilson.awssm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
